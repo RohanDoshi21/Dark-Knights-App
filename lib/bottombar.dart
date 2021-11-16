5fbdcb662845402707ef1dc0aed6403afca4bede
dineshNehete <nehetedinesh7@gmail.com>
2021-11-16 17:33:48 +0530

    App Drawer

---
+++ lib/bottombar.dart
@@ -0,0 +1,69 @@
+import 'package:darkknightspict/screens/Appoinments/appointments.dart';
+import 'package:darkknightspict/screens/Chat/chat_screen.dart';
+import 'package:darkknightspict/screens/Files/file_home.dart';
+import 'package:flutter/material.dart';
+// import 'package:hackdrawer/appointments.dart';
+// import 'package:hackdrawer/chat.dart';
+// import 'package:hackdrawer/drawer.dart';
+// import 'package:hackdrawer/home.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+  // static const routeName = 'BottomBar/';
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _pageIndex = 0;
+
+  PageController _pageController = PageController();
+  List<Widget> pages = [AppointmentsHome(), ChatScreen(), FileHome()];
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _pageIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _pageIndex,
+        onTap: gotoPage,
+        backgroundColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Appointment"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.messenger_outline_rounded), label: "Chat"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.file_upload_outlined), label: "Files"),
+        ],
+      ),
+      body: PageView(
+        children: pages,
+        onPageChanged: onPageChanged,
+        controller: _pageController,
+      ),
+    );
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      this._pageIndex = page;
+      print(page);
+    });
+  }
+
+  void gotoPage(int index) {
+    _pageController.animateToPage(index,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+}
---
+++ lib/developers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Developers extends StatelessWidget {
+  const Developers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Developers"),
+    );
+  }
+}
